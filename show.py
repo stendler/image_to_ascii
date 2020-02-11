5814405e24f2ac7183d9d63c387faa010ede5fcd
stendler <m.stendler@fu-berlin.de>
2020-02-11 19:12:05 +0100

    refactor arg usage, allow path to be specified

--- show.py
+++ show.py
@@ -1,31 +1,27 @@
 import glob
 import time
-import sys
+import argparse
 import os
 
 class Show:
-  arguments = sys.argv
-  delay = 0.07
+  args = None
 
   def __init__(self):
-    if len(self.arguments) == 1:
-      return
+    parser = argparse.ArgumentParser(description='Show computed ASCII image or gif.')
+    parser.add_argument('-d', '--delay', nargs='?', default=0.07, type=float, help='delay between frames, default is 0.07 seconds.')
+    parser.add_argument('folder', nargs='?', default='frames',type=str, help='folder containing the computed output.')
 
-    self.set_delay()
-
-  def set_delay(self):
-    for argument in self.arguments:
-      if argument == 's':
-        self.delay = float(self.arguments[self.arguments.index(argument) + 1])
+    self.args = parser.parse_args()
+    
 
   def clear_screen(self):
     print("\033[H\033[J")
 
   def show(self):
-    file_count = sum([len(files) for r, d, files in os.walk('frames')])
+    file_count = sum([len(files) for r, d, files in os.walk(self.args.folder)])
     while True:
-      files = glob.glob('frames/*')
-      files.sort(key = lambda x: float(x.strip('frames/output')))
+      files = glob.glob(self.args.folder + '/*')
+      files.sort(key = lambda x: float(x.strip(self.args.folder + '/output')))
 
       for f in files:
         with open(f, 'r') as content:
@@ -33,7 +29,7 @@ class Show:
           print(content.read())
         if file_count == 1:
           return
-        time.sleep(self.delay)
+        time.sleep(self.args.delay)
 
 if __name__ == '__main__':
   show = Show()
